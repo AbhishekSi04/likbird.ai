9157ed0fdcbb2fca4bd00f711107e61254d2504d
Abhishek Singh <abhishekabhi159084@gmail.com>
2025-09-10 20:55:46 +0530

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { Spinner, Google as GoogleIcon, Logo } from "@/components/ui/icons"
+import { Spinner, Logo } from "@/components/ui/icons"
 
 export default function LoginPage() {
   const router = useRouter();
@@ -96,21 +96,21 @@ export default function LoginPage() {
         </form>
         <CardFooter className="flex flex-col space-y-4">
           <div className="relative w-full">
-            <div className="absolute inset-0 flex items-center">
+{/*             <div className="absolute inset-0 flex items-center">
               <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
+            </div> */}
+{/*             <div className="relative flex justify-center text-xs uppercase">
               <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-            </div>
+            </div> */}
           </div>
-          <Button variant="outline" type="button" disabled={isLoading} className="w-full">
+{/*           <Button variant="outline" type="button" disabled={isLoading} className="w-full">
             {isLoading ? (
               <Spinner className="mr-2 h-4 w-4 animate-spin" />
             ) : (
               <GoogleIcon className="mr-2 h-4 w-4" />
             )}{' '}
             Google
-          </Button>
+          </Button> */}
           <p className="text-center text-sm text-muted-foreground">
             Don&apos;t have an account?{' '}
             <Link href="/register" className="font-medium text-primary hover:underline">
